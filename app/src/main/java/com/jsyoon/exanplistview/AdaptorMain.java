0b43190f5ee59491fff9044add26bc39654948aa
jsyoon <jsyoon@shinwooend.co.kr>
2017-04-10 15:41:57 +0900

    Adaptor에 필요한 getGroupView와 getChildView methods 추가 및 빠진 Override 함수 추가

--- app/src/main/java/com/jsyoon/exanplistview/AdaptorMain.java
+++ app/src/main/java/com/jsyoon/exanplistview/AdaptorMain.java
@@ -1,7 +1,12 @@
 package com.jsyoon.exanplistview;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,12 +16,12 @@ import java.util.Objects;
  * Created by ShinwooEND on 2017-04-10.
  */
 
-public class AdapterMain extends BaseExpandableListAdapter {
+public class AdaptorMain extends BaseExpandableListAdapter {
     private Context context;
     private ArrayList<String> arrayGroup;
     private HashMap<String, ArrayList<String>> arrayChild;
 
-    public  AdapterMain(Context context, ArrayList<String> arrayGroup, HashMap<String ,ArrayList<String>> arrayChild ){
+    public  AdaptorMain(Context context, ArrayList<String> arrayGroup, HashMap<String ,ArrayList<String>> arrayChild ){
         super();
         this.context = context;
         this.arrayGroup = arrayGroup;
@@ -27,12 +32,10 @@ public class AdapterMain extends BaseExpandableListAdapter {
     public int getGroupCount(){
         return arrayGroup.size();
     }
-
     @Override
     public int getChildrenCount(int groupPosition){
         return arrayChild.get(arrayGroup.get(groupPosition)).size();
     }
-
     @Override
     public Object getGroup(int groutPosition){
         return arrayGroup.get(groutPosition);
@@ -49,5 +52,42 @@ public class AdapterMain extends BaseExpandableListAdapter {
     public long getChildId(int groupPosition,int childPosition){
         return childPosition;
     }
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+    @Override
+    public boolean isChildSelectable(int groupPosition,int childPosition){
+        return false;
+    }
+    
+    // Adaptor에 필요한 getGroupView와 getChildView methods
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent){
+        String groupName = arrayGroup.get(groupPosition);
+        View v = convertView;
 
+        if(v == null) {
+            LayoutInflater inflator = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = (RelativeLayout)inflator.inflate(R.layout.lvi_group,null);
+        }
+        TextView textGroup = (TextView)v.findViewById(R.id.textGroup);
+        textGroup.setText(groupName);
+
+        return v;
+    }
+
+    public View getChildView(int groupPosition, int childPosition,boolean isLastChild, View convertView, ViewGroup parent){
+        String childName = arrayChild.get(arrayGroup.get(groupPosition)).get(childPosition);
+        View v = convertView;
+
+        if(v == null) {
+            LayoutInflater inflator = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = (RelativeLayout)inflator.inflate(R.layout.lvi_child,null);
+        }
+        TextView textChild = (TextView)v.findViewById(R.id.textChild);
+        textChild.setText(childName);
+
+        return v;
+    }
 }
